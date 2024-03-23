6b25b3a9d1149ad3df26621fc01921ceb0b34d99
Lucas Viana <luckasx@gmail.com>
2024-03-23 10:30:21 -0300

    Add Iberia, KLM and AirFrance

--- searcher.py
+++ searcher.py
@@ -10,7 +10,7 @@ def search(from_air, to_air, when_depart, when_return, search_days=0):
     when_return (string) date with YYYYMMDD format, 
     search_days (int) days forward to search
     '''
-    MAX_DAYS = 3
+    MAX_DAYS = 15
     counter = 0
 
     date_depart = datetime.strptime(when_depart, '%Y%m%d')
@@ -20,6 +20,14 @@ def search(from_air, to_air, when_depart, when_return, search_days=0):
 
         temp_depart = date_depart+timedelta(days=counter)
         temp_return = date_return+timedelta(days=counter)
+
+        depart_year = datetime.strftime(temp_depart, '%Y')
+        depart_month = datetime.strftime(temp_depart, '%m')
+        depart_day = datetime.strftime(temp_depart, '%d')
+
+        return_year = datetime.strftime(temp_return, '%Y')
+        return_month = datetime.strftime(temp_return, '%m')
+        return_day = datetime.strftime(temp_return, '%d')
 
         depart_yymmdd = datetime.strftime(temp_depart, '%y%m%d')
         depart_yyyy_mm_dd = datetime.strftime(temp_depart, '%Y-%m-%d')
@@ -33,7 +41,10 @@ def search(from_air, to_air, when_depart, when_return, search_days=0):
                 f"https://www.kayak.com.br/flights/{from_air}-{to_air}/{depart_yyyy_mm_dd}/{returning_yyyy_mm_dd}/2adults?sort=bestflight_a",
                 f"https://www.zupper.com.br/resultados?type=roundTrip&adultQty=1&childrenQty=0&infantQty=0&onlyBusinessClass=false&slices=%5B%7B%22originAirport%22:%22{from_air}%22,%22destinationAirport%22:%22{to_air}%22,%22departureDate%22:%22{depart_yyyy_mm_dd}%22%7D,%7B%22destinationAirport%22:%22{from_air}%22,%22originAirport%22:%22{to_air}%22,%22departureDate%22:%22{returning_yyyy_mm_dd}%22%7D%5D",
                 f"https://www.viajanet.com.br/shop/flights/results/roundtrip/{from_air}/{to_air}/{depart_yyyy_mm_dd}/{returning_yyyy_mm_dd}/1/0/0?from=SB&di=1&reSearch=true",
-                f"https://b2c.voegol.com.br/compra/busca-parceiros?pv=br&tipo=DF&de={from_air}&para={to_air}&ida={depart_mm_dd_yyyy}&volta={returning_mm_dd_yyyy}&ADT=1&CHD=0&INF=0"]
+                f"https://b2c.voegol.com.br/compra/busca-parceiros?pv=br&tipo=DF&de={from_air}&para={to_air}&ida={depart_mm_dd_yyyy}&volta={returning_mm_dd_yyyy}&ADT=1&CHD=0&INF=0",
+                f"https://www.iberia.com/flights/?market=BR&language=pt&appliesOMB=false&splitEndCity=false&initializedOMB=true&flexible=true&TRIP_TYPE=2&BEGIN_CITY_01={from_air}&END_CITY_01={to_air}&BEGIN_DAY_01={depart_day}&BEGIN_MONTH_01={depart_year}{depart_month}&BEGIN_YEAR_01={depart_year}&END_DAY_01={return_day}&END_MONTH_01={return_year}{return_month}&END_YEAR_01={return_year}&FARE_TYPE=R&quadrigam=IBHMPA&ADT=2&CHD=0&INF=0&BNN=0&YTH=0&YCD=0&residentCode=&familianumerosa=&BV_UseBVCookie=no&boton=Buscar&bookingMarket=BR#!/availability",
+                f"https://www.klm.com.br/en/search/offers?activeConnection=0&bookingFlow=LEISURE&cabinClass=ECONOMY&pax=1:0:0:0:0:0:0:0&connections={from_air}:A:{depart_year}{depart_month}{depart_day}%3E{to_air}:A-{to_air}:A:{return_year}{return_month}{return_day}%3E{from_air}:A",
+                f"https://wwws.airfrance.com.br/search/offers?activeConnection=0&bookingFlow=LEISURE&cabinClass=ECONOMY&pax=1:0:0:0:0:0:0:0&connections={from_air}:A:{depart_year}{depart_month}{depart_day}%3E{to_air}:A-{to_air}:A:{return_year}{return_month}{return_day}%3E{from_air}:A"]
 
         for url in urls:
             webbrowser.open(url)
